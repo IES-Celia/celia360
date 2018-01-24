0938357039996c2ce74eb66af55fec2abb981e15
pepeluchan <35689779+pepeluchan@users.noreply.github.com>
2018-01-24 13:14:28 +0100

    Update and rename escenas.php to Escenas.php

---
+++ application/controllers/Escenas.php
@@ -0,0 +1,79 @@
+<?php
+
+class escenas extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("Modeloescenas");
+    }
+    
+    
+    public function index(){
+        $this->showescenas();
+    }
+    public function showescenas() {
+        $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+        $this->load->view("escenas/Escenastable", $datos);
+    } 
+    
+    public function showinsert() {
+        $this->load->view("escenas/Insertar");
+    }
+    
+    public function processinsertscene(){
+        $resultado = $this->Modeloescenas->insertar();
+        if ($resultado == true) {
+            $datos["mensaje"] = "La inserci&oacute;n ha sido un &eacute;xito";
+            $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+            $this->load->view("escenas/Escenastable", $datos);
+        }
+        else {
+            $datos["error"] = "La inserci&oacute;n ha fallado";
+            $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+            $this->load->view("escenas/Escenastable", $datos);
+        }
+    }
+    
+    public function deletescene($id){
+
+        $resultado = $this->Modeloescenas->borrar($id);
+        
+        if ($resultado == 1) {
+            $datos["mensaje"] = "Escena borrado correctamente";
+            $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+            $this->load->view("escenas/Escenastable", $datos);
+            }
+        else {
+            $datos["error"] = "Error al borrar la escena";
+            $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+            $this->load->view("escenas/Escenastable",$datos);
+        }
+        
+    }
+    
+    public function showUpdateScene($id){
+    
+        $datos["tabla"]= $this->Modeloescenas->getOne($id);
+    
+        $this->load->view("escenas/Modificar", $datos);
+    }
+    
+    public function processUpdateScene($id){
+    
+            
+            $resultado = $this->Modeloescenas->update($id);
+            
+            if ($resultado == true) {
+                $datos["mensaje"] = "La modificaci&oacute;n ha sido un &eacute;xito";
+                $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+                $this->load->view("escenas/Escenastable", $datos);
+            }
+            else {
+                $datos["error"] = "La modificaci&oacute;n ha fallado";
+                $datos["tablaEscenas"] = $this->Modeloescenas->getAll();
+                $this->load->view("escenas/Escenastable", $datos);
+            }
+        
+    }
+}
+?>
