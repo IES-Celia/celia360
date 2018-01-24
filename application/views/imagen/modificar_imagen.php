a0f35f500115fe057de1ae60aed4bde2ec6ad27c
lolisalmeron <35336859+lolisalmeron@users.noreply.github.com>
2018-01-24 11:22:19 +0100

    Vistas de Imagen
    contiene:
    formulario_insertar_imagen, modificar_imagen y principal_img

---
+++ application/views/imagen/modificar_imagen.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No se permite el acceso directo al script');
+// Formulario para modificar la imagen
+if (isset($mensaje)) {
+    echo "<p style='color:green'>" . $mensaje . "</p>";
+}
+$du = $lista_imagenes[0];
+?>
+<h1 id="titulo">Modificar Imagen</h1>
+<!--<a class='insert' href='index.php?accion=listaImagenes'>Volver al listado</a>-->
+
+<a class='insert' href='<?php echo site_url("imagen/lista_imagenes"); ?>'>Volver al listado</a>
+<!-- CAMPOS DE LA TABLA : id_imagen,  titulo_imagen,  texto_imagen,  url_imagen , fecha -->
+<form class="for" enctype="multipart/form-data"  action='<?php echo site_url("imagen/actualizar_imagen"); ?>' method='post'>
+    <!--<form class="for" enctype="multipart/form-data" action='index.php' method='post'>-->
+    <?php
+    echo "<input type='hidden' name='id_imagen' value='" . $du['id_imagen'] . "'><br/>";
+    echo "T&iacute;tulo:<input type='text' name='titulo_imagen' value='" . $du['titulo_imagen'] . "'><br/>";
+    echo "<br>Texto:<input type='text' name='texto_imagen' value='" . $du['texto_imagen'] . "'><br/>";
+    echo '<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />';
+    echo "<br>Fecha:<input type='date' name='fecha'  value='" . $du['fecha'] . "'><br/>";
+    echo "<br>Imagen:<input type='file' id='imagen' name='imagen'value='" . $du['url_imagen'] . "'><br/>";
+    echo "<input type='hidden' name='url_imagen' value='" . $du['url_imagen'] . "'>";
+    echo "<img width='100px' src='".base_url("imagenes/" . $du['url_imagen']). "'><br><p>" . $du['url_imagen'] . "</p><br>";
+    ?>   
+    <input type='submit' name ='actualizar' value = 'Aceptar'>
+</form>
---
+++ application/views/imagen/principal_img.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No se permite el acceso directo al script');
+//formulario para mostrar la tabla imagenes, con sus datos 
+if (isset($mensaje)) {
+    echo "<p style='color:green'>" . $mensaje . "</p>";
+}
+// CAMPOS DE LA TABLA : id_imagen,  titulo_imagen,  texto_imagen,  url_imagen , fecha
+echo "<br><a class='insert' href='" . site_url("imagen/formulario_insertar_imagen") . "'>Insertar imagen</a><br>";
+echo "<a class='insert' href='" . site_url("imagen/imagen") . "'>Volver al Men&uacute;</a><br>";
+echo "<br><table id='cont' border='1'>";
+echo '<tr><th>Id</th><th>T&iacute;tulo</th><th>Texto</th><th>Url</th><th>Miniatura</th><th>Fecha</th><th>Borrar Imagen</th><th>Modificar Imagen</th></tr>';
+
+foreach ($lista_imagenes as $ima) {
+    $url_modificar = site_url("imagen/modificar_imagen/") . $ima["id_imagen"];
+    $url_borrar = site_url("imagen/borrar_imagen/") . $ima["id_imagen"];
+    echo "<tr><td>" . $ima["id_imagen"] . "</td><td>" . $ima["titulo_imagen"] . "</td><td>" . $ima["texto_imagen"] . "</td><td>" . $ima["url_imagen"] . "</td><td align='center'><img src='" . base_url("imagenes/" . $ima["url_imagen"]) . "' height='100px'></td><td>" . $ima["fecha"] . "</td>
+    	<td><a href='".$url_borrar."'>Borrar</a></td>
+    	<td><a href='" . $url_modificar . "'>Modificar</a></td></tr>";
+}
+echo "</table><br>";
