bc318a57813c2db27e20f1f759eb5b2466df6b7e
jamudi <35336546+jamudi@users.noreply.github.com>
2018-01-26 12:00:25 +0100

    Update Video.php

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -4,7 +4,7 @@
    class Video extends CI_Controller{
        
        public function __construct() {
-           
+           parent::__construct();
            $this->load->model("Vidm");
        }
        
