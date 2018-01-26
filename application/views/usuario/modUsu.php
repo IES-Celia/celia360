784f07f754ca609d5c04e3c87abc02fc94a6f68d
iescelia <33320343+iescelia@users.noreply.github.com>
2018-01-26 16:46:31 +0100

    Creo que ya si...

--- application/views/usuario/modUsu.php
+++ application/views/usuario/modUsu.php
@@ -8,8 +8,8 @@ echo "
     Nombre de usuario:<input type='text' name='username' value='".$DatosMod[0]["nombre_usuario"]."'><br/>
     Password:<input type='text' name='pass' value='".$DatosMod[0]["password"]."'><br/>
     Email:<input type='text' name='email' value='".$DatosMod[0]["email"]."'><br/>
-    Nombre:<input type='text' name='nombre' value='".$DatosMod[0]["Nombre"]."'><br/>
-    Apellidos:<input type='text' name='apellidos' value='".$DatosMod[0]["Apellido"]."'><br/>
+    Nombre:<input type='text' name='nombre' value='".$DatosMod[0]["nombre"]."'><br/>
+    Apellidos:<input type='text' name='apellidos' value='".$DatosMod[0]["apellido"]."'><br/>
     Foto Perfil:<input type='file' name='foto' value='".$DatosMod[0]["Foto"]."'><br/>
     <input type='hidden' name='id' value='".$DatosMod[0]["id_usuario"]."'><br/>
     <input type='submit'>
