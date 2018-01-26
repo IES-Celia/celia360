db42b8778885200b292ad5e5ae5c1d174b7c7f58
jamudi <35336546+jamudi@users.noreply.github.com>
2018-01-26 10:23:00 +0100

    Add files via upload

---
+++ application/controllers/Video.php
@@ -0,0 +1,57 @@
+<?php
+    // Este es el controlador de la aplicación
+   
+   class Video extends CI_Controller{
+       
+       public function __construct() {
+           parent::__construct();
+           $this->load->model("Vidm");
+       }
+       
+     public function index() {
+        $this->mostrarvideo();
+     }
+		
+	
+    public function frominsertarvideo(){
+		$this->load->view("video/Insertarvideos");	
+    }
+    
+   public function insertarvideo(){
+			$f_def =$_REQUEST["video"];
+			$desc=$_REQUEST["desc"];
+			$res=$this->Vidm->insertarvideo($desc, $f_def);
+			$da["tabla"]=$this->Vidm->buscarvideo();
+			$this->load->view("/video/Vvideos",$da);
+   }
+   public function mostrarvideo(){
+			$da["tabla"]=$this->Vidm->buscarvideo();
+			$this->load->view("video/Vvideos",$da);
+		
+   }
+   public function borrarvideo($id){
+			
+			$this->Vidm->borrarvideo($id);
+			$da["tabla"]=$this->Vidm->buscarvideo();
+			$this->load->view("video/Vvideos",$da);
+   }
+		
+   public function formmodificarvideo($id_vid){
+			
+			$da["vid"]=$this->Vidm->buscaridvideo($id_vid);
+			$this->load->view("video/Modificarvideos", $da);
+		
+   }
+   public function modificarvideo(){
+			$id=$_REQUEST["id"];
+			$this->Vidm->modificarvideo($id);
+			$da["tabla"]=$this->Vidm->buscarvideo();
+			$this->load->view("video/Vvideos", $da);
+		
+   }
+        
+    
+    
+
+}
+?>
