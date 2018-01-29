c96303518da653b3bea6d4ea9e8927cae0ffb0a9
iescelia <33320343+iescelia@users.noreply.github.com>
2018-01-29 12:12:18 +0100

    Movimiento naranja!!

--- application/views/usuario/mapero.php
+++ application/views/usuario/mapero.php
@@ -5,19 +5,19 @@ echo "Hola, ".$datos["nombreUsuario"];
 
 echo "<table border='1'>";
     echo "<tr>
-			<td><a href='".site_url("Hotspot")."''>Hotspot</a></td>
+			<td><a href='".site_url("hotspot")."''>Hotspot</a></td>
 		</tr>
 		<tr>
-			<td><a href='".site_url("Escenas")."'>Escenas</a></td>
+			<td><a href='".site_url("escenas")."'>Escenas</a></td>
 		</tr>
 		<tr>
-			<td><a href='".site_url("Audio")."'>Audio</a></td>
+			<td><a href='".site_url("audio")."'>Audio</a></td>
 		</tr>
 		<tr>
-			<td><a href='".site_url("Imagen")."'>Imagenes</a></td>
+			<td><a href='".site_url("imagen")."'>Imagenes</a></td>
 		</tr>
 		<tr>
-			<td><a href='".site_url("Video")."'>Video</a></td>
+			<td><a href='".site_url("video")."'>Video</a></td>
 		</tr>";
 
 echo "</table>";
