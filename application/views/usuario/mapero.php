03ac6815ac471fd93fd315700566cef1497de997
iescelia <33320343+iescelia@users.noreply.github.com>
2018-01-29 12:08:36 +0100

    Cambiado acceso al menu de mapero

--- application/views/usuario/mapero.php
+++ application/views/usuario/mapero.php
@@ -5,19 +5,19 @@ echo "Hola, ".$datos["nombreUsuario"];
 
 echo "<table border='1'>";
     echo "<tr>
-			<td><a href='Hotspot/'>Hotspot</a></td>
+			<td><a href='".site_url("Hotspot")."''>Hotspot</a></td>
 		</tr>
 		<tr>
-			<td><a href='Escenas/'>Escenas</a></td>
+			<td><a href='".site_url("Escenas")."'>Escenas</a></td>
 		</tr>
 		<tr>
-			<td><a href='Audio/'>Audio</a></td>
+			<td><a href='".site_url("Audio")."'>Audio</a></td>
 		</tr>
 		<tr>
-			<td><a href=''>Imagenes</a></td>
+			<td><a href='".site_url("Imagen")."'>Imagenes</a></td>
 		</tr>
 		<tr>
-			<td><a href=''>Video</a></td>
+			<td><a href='".site_url("Video")."'>Video</a></td>
 		</tr>";
 
 echo "</table>";
