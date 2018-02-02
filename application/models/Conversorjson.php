0f6d77c528dfb8a62f8def56c1563854c2886013
miguelille <32550594+miguelille@users.noreply.github.com>
2018-02-02 14:21:07 +0100

    Añadido plataforma json

--- application/models/Conversorjson.php
+++ application/models/Conversorjson.php
@@ -95,7 +95,7 @@ class Conversorjson extends CI_Model {
         $json = $json . '"pitch": '.$escena['pitch'].',';  
         $json = $json . '"yaw": '.$escena['yaw'].',';  
         $json = $json . '"type": "equirectangular",';   
-        $json = $json . '"panorama": "'.$escena['panorama'].'",';  
+        $json = $json . '"panorama": "'.base_url("assets/imagenes/escenas/".$escena['panorama']).'",';  
         $json = $json . '"hotSpots": ['; // ese $escena[hotspot] no tiene sentido pero está gracioso        
                 
         $sql = "SELECT * FROM hotspots INNER JOIN escenas_hotspots ON hotspots.id_hotspot = escenas_hotspots.id_hotspot WHERE escenas_hotspots.id_escena = ".$escena['id_escena'];
@@ -125,6 +125,56 @@ class Conversorjson extends CI_Model {
     $json = $json . '}';  
     return $json;
   }
+    
+    
+    public function get_datos_plataforma() {
+    $this->load->database();
+    $sql = "SELECT * FROM escenas";
+    $res = $this->db->query($sql);
+    $flagObj=false;
+    $flagHot=false;
+    $json = ' {"default": {"firstScene": "p1p2","sceneFadeDuration": 1000,"autoLoad": true,"showControls": false,"compass":false,"preview": "preview22.png","hotSpotDebug": true },  "scenes": {';
+    foreach ($res->result_array() as $escena) {
+        if($flagObj){ // con esta comprobacion pondrá coma entre objetos escenas sin ponerla en el primero
+            $json = $json . ',';
+        }
+        $flagObj= true;
+        
+        $json = $json . ' "' .$escena['cod_escena'].'"'; 
+        $json = $json . ': { ';
+        $json = $json . '"hfov": 120,'; 
+        $json = $json . '"pitch": '.$escena['pitch'].',';  
+        $json = $json . '"yaw": '.$escena['yaw'].',';  
+        $json = $json . '"type": "equirectangular",';   
+        $json = $json . '"panorama": "'.base_url("assets/imagenes/escenas/".$escena['cod_escena']).'.JPG",'; 
+        $json = $json . '"hotSpots": ['; // ese $escena[hotspot] no tiene sentido pero está gracioso        
+                
+        $sql = "SELECT * FROM hotspots INNER JOIN escenas_hotspots ON hotspots.id_hotspot = escenas_hotspots.id_hotspot WHERE escenas_hotspots.id_escena = ".$escena['id_escena'];
+        $res2 = $this->db->query($sql);
+        foreach ($res2->result_array() as $hotspot) {
+            if($flagHot){
+                $json = $json . ',';
+            }
+            $flagHot= true;
+              
+            $json = $json . '{"pitch": '.$hotspot['pitch'].','; 
+            $json = $json . '"yaw": '.$hotspot['yaw'].',';  
+            $json = $json . '"type": "'.$hotspot['tipo'].'",'; 
+            $json = $json . '"cssClass": "'.$hotspot['cssClass'].'",';  
+            $json = $json . '"clickHandlerFunc": "modificarHotspot",';  
+            $json = $json . '"clickHandlerArgs": "'.$hotspot['id_hotspot'].'"';
+            $json = $json . '} '; 
+           
+        }
+        $flagHot=false;
+        $json = $json . ']}';  
+    }
+
+    $json = $json . '}';  
+    $json = $json . '}';  
+    return $json;
+        
+    }
   
 }
 ?>
