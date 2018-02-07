5560486883538cce068506055ff6112ce405d022
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2018-02-07 19:36:29 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,4 @@
-<?php
+ia<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/php/celia360/';
+$config['base_url'] = 'http://iesceliaciclos.org/celia360/';
 
 /*
 |--------------------------------------------------------------------------
