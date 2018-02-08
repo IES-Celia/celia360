2687fd0287ee66299cfe479e0b908d444065a345
miguelille <32550594+miguelille@users.noreply.github.com>
2018-02-08 10:59:57 +0100

    Jotpoch's insertator 2.0

--- application/views/hotspots/insertHotspot.php
+++ application/views/hotspots/insertHotspot.php
@@ -1,57 +1,35 @@
 <html>
 	<head>
-	<style type="text/css">
 
-.button {
-    background-color: #555555; /* Black	*/
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-	
-	}
-	
-div.centrado {
-	margin-left:25%;
-	margin-right:25%;
-	
-}
-
-</style>
-		<title> Insert Hotspot </title>
+     <script>
+        
+        
+        
+         
+    </script>   
+        
+<title> Insert Hotspot </title>
 </head>
 <body>
+    
+    
 <h1> Formulario para Insertar Hotspots </h1>
 
-<fieldset class="for">
-
-<?php
-echo "<form action='".   site_url("hotspots/process_insert_hotspot")   ."' method='get'>";
-?>
-	Descripción:  <input type='text' name='descripcion'> </br> </br>
-	
-	Coordenada Pitch: <input type='text' name='pitch'> </br> </br>
-	
-	Coordenada Yaw: <input type='text' name='yaw'> </br> </br>
-	
-	cssClass: <input type='text' name='cssClass'> </br> </br>
-	
-	clickHandlerFunc: <input type='text' name='clickHandlerFunc'> </br> </br>
-	
-	clickHandlerArgs: <input type='text' name='clickHandlerArgs'> </br> </br>
-	
-	sceneId: <input type='text' name='sceneId'> </br> </br>
-	
-	targetPitch: <input type='text' name='targetPitch'> </br> </br>
-	
-	targetYaw: <input type='text' name='targetYaw'> </br> </br>
-	
-	Tipo: <input type='text' name='tipo'> </br> </br> 
-	
-
-	<input type='submit' class="button">
-	
-</fieldset>
+<div id="puntoScene"> 
+    <?php
+    echo "<form action='".   site_url("hotspots/process_insert_hotspot")   ."' method='get'>"; ?>
+        Descradasdasdasipción:  <input type='text' name='descripcion'   ><br> 
+        Coordenada Pitch: <input type='text' name='pitch' value=' <?php echo $pitch ?> '><br> 
+        Coordenada Yaw: <input type='text' name='yaw 'value=' <?php echo $yaw ?> '><br> 
+        cssClass: <input type='text' name='cssClass' value='custom-hotspot-salto' readonly="readonly"><br> 
+        Tipo: <input type='text' name='tipo' value='scene' readonly="readonly"> <br>
+        clickHandlerFunc: <input type='text' name='clickHandlerFunc' value='puntos' readonly="readonly"><br> 
+        clickHandlerArgs: <input type='text' name='clickHandlerArgs'><br> 
+        sceneId: <input type='text' name='sceneId'><br>
+        targetPitch: <input type='text' name='targetPitch' ><br>
+        targetYaw: <input type='text' name='targetYaw'><br>
+        <input type='submit' class="button">
+    </form>
+</div>
+    
+    
