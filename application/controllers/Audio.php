bb07d7d492c91449acd51d14a66597a0455861d0
lolisalmeron <35336859+lolisalmeron@users.noreply.github.com>
2018-02-09 10:08:55 +0100

    seguridad para los datos de entrada. Input Class
    cambio de  ejemplo:
    $tipo=$_REQUEST["tipo_aud"];
    por
    $tipo = $this->input->post_get("tipo_aud");

--- application/controllers/Audio.php
+++ application/controllers/Audio.php
@@ -1,83 +1,77 @@
 <?php
+
 // Este es el controlador de la aplicación
-   defined('BASEPATH') OR exit('No se permite el acceso directo al script');
-   class Audio extends CI_Controller{
-       
-       public function __construct() {
-           parent::__construct();
-           $this->load->model("Audm");
-           $this->load->model("UsuarioModel");
-       }
-       
-     public function index() {
+defined('BASEPATH') OR exit('No se permite el acceso directo al script');
+
+class Audio extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("Audm");
+        $this->load->model("UsuarioModel");
+    }
+
+    public function index() {
         $this->mostraraudios();
-     }
-       
-     public function forminsertaraudio(){
-        $datos["vista"]="audio/Insertaraudios";
-        $datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
+    }
+
+    public function forminsertaraudio() {
+        $datos["vista"] = "audio/Insertaraudios";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
         $this->load->view("template_admin", $datos);
-     }
-        
-     public function insertaraud(){   
-            $f_def = "audios/".$_FILES["audio"]["name"];
-            $r=$this->Audm->existeaud($f_def);
-            
-            if ($r==true){
-                echo"el archivo ya existe en el servidor, intenta cambiarle el nombre antes de subirlo si no quieres qe se sobreescriba";
-                echo"</br><a href='".site_url("audio/forminsertaraudio")."'>insertar</a></br>";
-                echo"</br><a href='".site_url("audio/mostraraudios")."'>mostrar</a></br>";
-                echo"</br><a href='".site_url("audio/mostraraudios")."'>renombrar archivo en el servidor</a></br>";
-            }else{
-            $tipo=$_REQUEST["tipo_aud"];
-            $desc=$_REQUEST["desc"];
-            $res=$this->Audm->insertaraud($desc, $tipo);
-            $datos["tabla"]=$this->Audm->buscaraud();
-            $datos["vista"]="audio/Vaudios";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-            $this->load->view("template_admin",$datos);
-        }
-        
-        
-     }
-        
-        public function mostraraudios(){
-            $datos["tabla"]=$this->Audm->buscaraud();
-            $datos["vista"]="audio/Vaudios";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-            $this->load->view("template_admin",$datos);
-        
-        }
-        
-        public function borraraud($id){
-            $this->Audm->borraraud($id);
-            $datos["tabla"]=$this->Audm->buscaraud();
-            $datos["vista"]="audio/Vaudios";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-            $this->load->view("template_admin",$datos);
-        }
-        
-        public function formmodificarAud($id_aud){
-            $datos["aud"]=$this->Audm->buscaridaud($id_aud);
-            $datos["vista"]="audio/Modificaraudios";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
+    }
+
+    public function insertaraud() {
+        $f_def = "audios/" . $_FILES["audio"]["name"];
+        $r = $this->Audm->existeaud($f_def);
+
+        if ($r == true) {
+            echo"el archivo ya existe en el servidor, intenta cambiarle el nombre antes de subirlo si no quieres qe se sobreescriba";
+            echo"</br><a href='" . site_url("audio/forminsertaraudio") . "'>insertar</a></br>";
+            echo"</br><a href='" . site_url("audio/mostraraudios") . "'>mostrar</a></br>";
+            echo"</br><a href='" . site_url("audio/mostraraudios") . "'>renombrar archivo en el servidor</a></br>";
+        } else {
+            $tipo = $this->input->post_get("tipo_aud");
+            $desc = $this->input->post_get("desc");
+            $res = $this->Audm->insertaraud($desc, $tipo);
+            $datos["tabla"] = $this->Audm->buscaraud();
+            $datos["vista"] = "audio/Vaudios";
+            $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
             $this->load->view("template_admin", $datos);
-        
         }
+    }
 
-        public function modificaraud(){
-            $id=$_REQUEST["id"];
-            $this->Audm->modificaraud($id);
-            $datos["tabla"]=$this->Audm->buscaraud();
-            $datos["vista"]="audio/Vaudios";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-            $this->load->view("template_admin", $datos);
-        
-   }
+    public function mostraraudios() {
+        $datos["tabla"] = $this->Audm->buscaraud();
+        $datos["vista"] = "audio/Vaudios";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view("template_admin", $datos);
+    }
 
+    public function borraraud($id) {
+        $this->Audm->borraraud($id);
+        $datos["tabla"] = $this->Audm->buscaraud();
+        $datos["vista"] = "audio/Vaudios";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view("template_admin", $datos);
+    }
 
-    
-    
-   }
+    public function formmodificarAud($id_aud) {
+        $datos["aud"] = $this->Audm->buscaridaud($id_aud);
+        $datos["vista"] = "audio/Modificaraudios";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view("template_admin", $datos);
+    }
+
+    public function modificaraud() {
+        $id = $this->input->post_get("id");
+        $this->Audm->modificaraud($id);
+        $datos["tabla"] = $this->Audm->buscaraud();
+        $datos["vista"] = "audio/Vaudios";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view("template_admin", $datos);
+    }
+
+}
 
 ?>
--- application/models/Audm.php
+++ application/models/Audm.php
@@ -1,94 +1,93 @@
 <?php
-        
-    class AudM extends CI_Model{
-            public function __construct() {
-                parent::__construct();
-                $this->load->database();
-            }
 
-        public function insertaraud($desc, $tipo){
-            
-            $r="";
-            
-            $st=$_FILES["audio"]["name"];
-            $f_def = "audios/".$_FILES["audio"]["name"];
-           if (move_uploaded_file($_FILES['audio']['tmp_name'], $f_def)) {
-            echo "El fichero es válido y se subió con éxito.\n";
-            $insrt="insert into audio (url_aud,desc_aud, tipo_aud)values('$f_def','$desc','$tipo')";
-            $r=$this->db->query($insrt);
-            } else {
-            echo "¡Posible ataque de subida de ficheros!\n".$_FILES["audio"]["error"];
-            }
-          
-            return $r;  
-        }
-    
-        public function buscaraud(){
-          
-            $sel="select * from audio";
-            $res = $this->db->query($sel);
-            $tabla = array();
-            foreach ($res->result_array() as $fila) {
-                $tabla[] = $fila;
-            }
-            
-            return $tabla;
-        }
-    
-        public function borraraud($id_aud){
-            $s="select url_aud from audio where id_aud='$id_aud'";
-            $res=$this->db->query($s);
-            $nom = $res->result_array()[0]["url_aud"];
-            unlink($nom);
-            $del="delete from audio where id_aud='$id_aud'";
-            $alter="alter table audio auto_increment=1;";
-            $r=$this->db->query($del);
-            $this->db->query($alter);
-            
-        return $r;
-        }
-    
-        public function modificaraud($id){
-            $url=$_REQUEST["url_aud"];
-            $desc=$_REQUEST["desc_aud"];
-            $tipo=$_REQUEST["tipo_aud"];
-           
-            $url1="audios/".$url.".mp3";
-            
-            $s="select url_aud from audio where id_aud='$id'";
-            $res=$this->db->query($s);
-            $n = $res->result_array()[0]["url_aud"];
-            rename($n,$url1);
-            $del="update audio set url_aud='$url1', desc_aud='$desc', tipo_aud='$tipo' where id_aud='$id'";
-            $r=$this->db->query($del);
-            
-        return $r;
-        }
-    
-        public function buscaridaud($id){
-           
-            $sel="select * from audio where id_aud='$id'";
-            $res= $this->db->query($sel);
-            $tabla = array();
-            foreach ($res->result_array() as $fila) {
-                $tabla[] = $fila;
-            }
-    
-        return $tabla;
-        }
+class AudM extends CI_Model {
 
-        public function existeaud($n){
-            
-            
-            $s="select url_aud from audio where url_aud='$n'";
-            $a=$this->db->query($s);
-            if($a->num_rows())
-                return true;
-            else 
-                return false;  
-        }
-		
-
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
     }
-    
-   ?>
+
+    public function insertaraud($desc, $tipo) {
+
+        $r = "";
+
+        $st = $_FILES["audio"]["name"];
+        $f_def = "audios/" . $_FILES["audio"]["name"];
+        if (move_uploaded_file($_FILES['audio']['tmp_name'], $f_def)) {
+            echo "El fichero es válido y se subió con éxito.\n";
+            $insrt = "insert into audio (url_aud,desc_aud, tipo_aud)values('$f_def','$desc','$tipo')";
+            $r = $this->db->query($insrt);
+        } else {
+            echo "¡Posible ataque de subida de ficheros!\n" . $_FILES["audio"]["error"];
+        }
+
+        return $r;
+    }
+
+    public function buscaraud() {
+
+        $sel = "select * from audio";
+        $res = $this->db->query($sel);
+        $tabla = array();
+        foreach ($res->result_array() as $fila) {
+            $tabla[] = $fila;
+        }
+
+        return $tabla;
+    }
+
+    public function borraraud($id_aud) {
+        $s = "select url_aud from audio where id_aud='$id_aud'";
+        $res = $this->db->query($s);
+        $nom = $res->result_array()[0]["url_aud"];
+        unlink($nom);
+        $del = "delete from audio where id_aud='$id_aud'";
+        $alter = "alter table audio auto_increment=1;";
+        $r = $this->db->query($del);
+        $this->db->query($alter);
+
+        return $r;
+    }
+
+    public function modificaraud($id) {
+        $url = $this->input->post_get("url_aud");
+        $desc = $this->input->post_get("desc_aud");
+        $tipo = $this->input->post_get("tipo_aud");
+
+        $url1 = "audios/" . $url . ".mp3";
+
+        $s = "select url_aud from audio where id_aud='$id'";
+        $res = $this->db->query($s);
+        $n = $res->result_array()[0]["url_aud"];
+        rename($n, $url1);
+        $del = "update audio set url_aud='$url1', desc_aud='$desc', tipo_aud='$tipo' where id_aud='$id'";
+        $r = $this->db->query($del);
+
+        return $r;
+    }
+
+    public function buscaridaud($id) {
+
+        $sel = "select * from audio where id_aud='$id'";
+        $res = $this->db->query($sel);
+        $tabla = array();
+        foreach ($res->result_array() as $fila) {
+            $tabla[] = $fila;
+        }
+
+        return $tabla;
+    }
+
+    public function existeaud($n) {
+
+
+        $s = "select url_aud from audio where url_aud='$n'";
+        $a = $this->db->query($s);
+        if ($a->num_rows())
+            return true;
+        else
+            return false;
+    }
+}
+
+?>
