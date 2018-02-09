4367449553287819c1c3eecd4eff1452a7a79f40
lolisalmeron <35336859+lolisalmeron@users.noreply.github.com>
2018-02-09 10:21:05 +0100

    seguridad datos de entrada. Input Class
    video, ejemplo cambio:
    $f_def =$_REQUEST["video"];
    por
     $f_def = $this->input->post_get("video");

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -1,70 +1,68 @@
 <?php
-    // Este es el controlador de la aplicación
-   
-   class Video extends CI_Controller{
-       
-       public function __construct() {
-           parent::__construct();
-           $this->load->model("Vidm");
-		   $this->load->model("UsuarioModel");
-       }
-       
-     public function index() {
-        $this->mostrarvideo();
-     }
-		
-	
-    public function frominsertarvideo(){
-        $datos["vista"] ="video/Insertarvideos";
-		$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-		$this->load->view('template_admin', $datos);	
-    }
-    
-   public function insertarvideo(){
-			$f_def =$_REQUEST["video"];
-			$desc=$_REQUEST["desc"];
-			$res=$this->Vidm->insertarvideo($desc, $f_def);
-			$datos["tabla"]=$this->Vidm->buscarvideo();
-            $datos["vista"]="video/Vvideos";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-			$this->load->view('template_admin',$datos);
-   }
-   public function mostrarvideo(){
-			$datos["tabla"]=$this->Vidm->buscarvideo();
-            $datos["vista"]="video/Vvideos";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-			$this->load->view('template_admin',$datos);
-		
-   }
-   public function borrarvideo($id){
-			
-			$this->Vidm->borrarvideo($id);
-			$datos["tabla"]=$this->Vidm->buscarvideo();
-            $datos["vista"]="video/Vvideos";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-			$this->load->view('template_admin',$datos);
-   }
-		
-   public function formmodificarvideo($id_vid){
-			
-			$datos["vid"]=$this->Vidm->buscaridvideo($id_vid);
-            $datos["vista"]="video/Modificarvideos";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-			$this->load->view('template_admin', $datos);
-		
-   }
-   public function modificarvideo(){
-			$id=$_REQUEST["id"];
-			$this->Vidm->modificarvideo($id);
-			$datos["tabla"]=$this->Vidm->buscarvideo();
-            $datos["vista"]="video/Vvideos";
-			$datos["permiso"]=$this->UsuarioModel->comprueba_permisos($datos["vista"]);
-			$this->load->view('template_admin', $datos);
-		
-   }
-        
-    
-    
 
+// Este es el controlador de la aplicación
+
+class Video extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model("Vidm");
+        $this->load->model("UsuarioModel");
+    }
+
+    public function index() {
+        $this->mostrarvideo();
+    }
+
+    public function frominsertarvideo() {
+        $datos["vista"] = "video/Insertarvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
+
+    public function insertarvideo() {
+        $f_def = $this->input->post_get("video");
+        $desc = $this->input->post_get("desc");
+        $res = $this->Vidm->insertarvideo($desc, $f_def);
+        $datos["tabla"] = $this->Vidm->buscarvideo();
+        $datos["vista"] = "video/Vvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
+
+    public function mostrarvideo() {
+        $datos["tabla"] = $this->Vidm->buscarvideo();
+        $datos["vista"] = "video/Vvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
+
+    public function borrarvideo($id) {
+
+        $this->Vidm->borrarvideo($id);
+        $datos["tabla"] = $this->Vidm->buscarvideo();
+        $datos["vista"] = "video/Vvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
+
+    public function formmodificarvideo($id_vid) {
+
+        $datos["vid"] = $this->Vidm->buscaridvideo($id_vid);
+        $datos["vista"] = "video/Modificarvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
+
+    public function modificarvideo() {
+
+        $id = $this->input->post_get("id");
+        $this->Vidm->modificarvideo($id);
+        $datos["tabla"] = $this->Vidm->buscarvideo();
+        $datos["vista"] = "video/Vvideos";
+        $datos["permiso"] = $this->UsuarioModel->comprueba_permisos($datos["vista"]);
+        $this->load->view('template_admin', $datos);
+    }
 }
+
 ?>
--- application/views/video/Vvideos.php
+++ application/views/video/Vvideos.php
@@ -3,20 +3,18 @@
 echo"<table align='center' border=1 id='cont'><tr>
 <th>id</th>
 <th>URL</th>
-<th>descrepcion</th>
+<th>descripcion</th>
 <th>modificar</th>
-<th>Eleminar</th></tr>
+<th>Eliminar</th></tr>
 ";
 
-foreach ($tabla as $re){
-	echo'<tr><td>'.$re["id_vid"].'</td>';
-	echo'<td>'.$re["url_vid"].'</td>';
-	echo'<td>'.$re["desc_vid"].'</td>';
-	echo"<td><a href='".site_url("video/formmodificarvideo/".$re["id_vid"])."'>modificar</a></td>";
-	echo"<td><a href='".site_url("video/borrarvideo/".$re["id_vid"])."'>Eleminar</a></td></tr>";
-
-	
+foreach ($tabla as $re) {
+    echo'<tr><td>' . $re["id_vid"] . '</td>';
+    echo'<td>' . $re["url_vid"] . '</td>';
+    echo'<td>' . $re["desc_vid"] . '</td>';
+    echo"<td><a href='" . site_url("video/formmodificarvideo/" . $re["id_vid"]) . "'>modificar</a></td>";
+    echo"<td><a href='" . site_url("video/borrarvideo/" . $re["id_vid"]) . "'>Eliminar</a></td></tr>";
 }
 echo "</table>";
-echo"<a class='insert' href='".site_url("Video/frominsertarvideo")."'>insertar</a>";
+echo"<a class='insert' href='" . site_url("Video/frominsertarvideo") . "'>insertar</a>";
 ?>
