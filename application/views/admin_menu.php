2a18b21006d89a5dfd5b9fa6aa1bd41998f9bc4f
Dansberg <dansbergrach@gmail.com>
2018-02-13 12:27:05 +0100

    Pues funciono

--- application/views/admin_menu.php
+++ application/views/admin_menu.php
@@ -34,27 +34,3 @@
 
     
 
-
-<p>
-    <table align="center" border= '2px solid' id="menu">
-    
-    <tr>
-        
-    <td><a href="<?php echo site_url("imagen");?>">Administrar imágenes</a></td>
-    
-    <td><a href="<?php echo site_url("audio");?>">Administrar audios</a></td>
-    
-    <td><a href="<?php echo site_url("video");?>">Administrar vídeos</a></td>
-    
-    <td><a href="<?php echo site_url("escenas");?>">Administrar escenas</a></td>
-    
-    <td><a href="<?php echo site_url("hotspots");?>">Administrar hotspots</a></td>
-    
-    <td><a href="<?php echo site_url("usuario/usuarios");?>">Administrar usuarios</a></td>
-    
-    <td><a href="<?php echo site_url("biblioteca");?>">Administrar biblioteca</a></td>
-    
-    </tr>
-    
-    </table>
-</p>
