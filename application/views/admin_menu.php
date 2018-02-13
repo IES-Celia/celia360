b963826243abeeb64ed1d67e4e3bea22bca81965
Dansberg <dansbergrach@gmail.com>
2018-02-13 12:24:12 +0100

    A ver si va

--- application/views/admin_menu.php
+++ application/views/admin_menu.php
@@ -20,13 +20,13 @@
         
         <input type="text" placeholder="  Buscar..."><i class="fa fa-search" style="color: white; font-size: 22px; cursor: pointer; position: absolute; right: 27px; top:70px;;"></i>
         <br><br><br>
-        <a href="#" class="enlacesidenav">Administrar Imagenes</a>
-        <a href="#" class="enlacesidenav">Administrar Audios</a>
-        <a href="#" class="enlacesidenav">Administrar Vídeos</a>
-        <a href="#" class="enlacesidenav">Administrar Escenas</a>
-        <a href="#" class="enlacesidenav">Administrar Hotspots</a>
-        <a href="#" class="enlacesidenav">Administrar Usuarios</a>
-        <a href="#" class="enlacesidenav">Administrar Bibilioteca </a>
+        <a href="<?php echo site_url("imagen");?>" class="enlacesidenav">Administrar Imagenes</a>
+        <a href="<?php echo site_url("audio");?>" class="enlacesidenav">Administrar Audios</a>
+        <a href="<?php echo site_url("video");?>" class="enlacesidenav">Administrar Vídeos</a>
+        <a href="<?php echo site_url("escenas");?>" class="enlacesidenav">Administrar Escenas</a>
+        <a href="<?php echo site_url("hotspots");?>" class="enlacesidenav">Administrar Hotspots</a>
+        <a href="<?php echo site_url("usuario/usuarios");?>" class="enlacesidenav">Administrar Usuarios</a>
+        <a href="<?php echo site_url("biblioteca");?>" class="enlacesidenav">Administrar Bibilioteca </a>
         
     </div>
     
