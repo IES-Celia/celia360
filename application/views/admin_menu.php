6a92d95d6dde269230fcd4643e70d7653a80899a
Dansberg <dansbergrach@gmail.com>
2018-02-13 11:55:45 +0100

    CSS de las tablas cambiado
    No esta bien aun

--- application/views/admin_menu.php
+++ application/views/admin_menu.php
@@ -1,3 +1,40 @@
+    
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+        
+    </script>
+     
+    
+   
+
+
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          
+        
+        <input type="text" placeholder="  Buscar..."><i class="fa fa-search" style="color: white; font-size: 22px; cursor: pointer; position: absolute; right: 27px; top:70px;;"></i>
+        <br><br><br>
+        <a href="#" class="enlacesidenav">Administrar Imagenes</a>
+        <a href="#" class="enlacesidenav">Administrar Audios</a>
+        <a href="#" class="enlacesidenav">Administrar Vídeos</a>
+        <a href="#" class="enlacesidenav">Administrar Escenas</a>
+        <a href="#" class="enlacesidenav">Administrar Hotspots</a>
+        <a href="#" class="enlacesidenav">Administrar Usuarios</a>
+        <a href="#" class="enlacesidenav">Administrar Bibilioteca </a>
+        
+    </div>
+    
+    <span class="sidenavmenu" onclick="openNav()">&#9776; Menu </span>
+
+    
+
+
 <p>
     <table align="center" border= '2px solid' id="menu">
     
