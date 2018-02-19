d5d5b046e6a25e10863fa5dda4270c91187ca070
pepeluchan <jose.luis.ramirez56@gmail.com>
2018-02-19 17:59:14 +0100

    isn

--- application/views/escenas/Insertar.php
+++ application/views/escenas/Insertar.php
@@ -7,7 +7,7 @@
 	<fieldset class='for'>
 
 		<?php
-			echo"<form action='".site_url("escenas/processinsertscene")."' method='post' enctype='multipart/form-data'>";
+			echo"<form action='".site_url("escenas/processinsertscene")."' method='get' enctype='multipart/form-data'>";
 
 			echo "<input type='hidden' value='$id_mapa' name='id_mapa'>";
 			echo "<input type='hidden' value='$left_mapa' name='left_mapa'>";
@@ -16,7 +16,7 @@
 		    	
 				Nombre del lugar:  <input type='text' name='name'> <br/>
         
-				Panorama:  <input type='file' name='panorama' placeholder="Seleccione la escena"> <br/>
+				Panorama:  <input type='file' name='panorama' placeholder='Seleccione la escena'> <br/>
 			    
 			    
 				<input type='submit'>
