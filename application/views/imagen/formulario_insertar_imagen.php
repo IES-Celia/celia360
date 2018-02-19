f9434e0b82ca5a0924f4a27690036754bec296f6
lolisalmeron <35336859+lolisalmeron@users.noreply.github.com>
2018-02-19 08:28:25 +0100

    retoques

--- application/views/imagen/formulario_insertar_imagen.php
+++ application/views/imagen/formulario_insertar_imagen.php
@@ -7,46 +7,38 @@ if (isset($mensaje)) {
 if (isset($error)) {
     echo "<p style='color:red'>" . $error . "</p>";
 }
-?><!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Proyecto 360 CodeIgniter</title>
-    </head>
-    <body>
-        <!-- CAMPOS DE LA TABLA : id_imagen,  titulo_imagen,  texto_imagen,  url_imagen , fecha -->
-        <!-- AQUI EMPIEZA LA VISTA -->
-        <section  id="ContenedorImagen">
-            <header>
-                <h1 id="titulo">Insertar imagen</h1>
-            </header>
-            <p>
-                <a class='insert' href='<?php echo site_url("imagen/lista_imagenes"); ?>'>Volver al listado</a>
-            <form class="for" enctype="multipart/form-data" action='<?php echo site_url("imagen/insertar_imagen"); ?>' method="post">
-                <input type='hidden' name='accion' value='insertar_imagen'>
-                <div>
-                    <input id= "id_imagen" name='id_imagen' type ="hidden"><br />
-                    <label id= "label_titulo" for="titulo">T&iacute;tulo:</label>
-                    <input id="titulo" name='titulo_imagen' placeholder="Introduzca el t&iacute;tulo" required><br />
-                </div>
-                <div>
-                    <label for="texto_imagen">Texto:</label>
-                    <textarea id="texto_imagen" name='texto_imagen' placeholder="Introduzca la descripci&oacute;n de la imagen"></textarea><br>
-                </div>
-                <div>
-                    <label for="fecha">Fecha:</label>
-                    <input type="date" id="fecha" name='fecha' placeholder="Introduzca la fecha" value="<?php echo date("Y-m-d");?>" required><br />
-                </div>
-                <div>
-                    <!-- MAX_FILE_SIZE debe preceder al campo de entrada del fichero -->
-                    <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
-                    <label for="imagen">Imagen:</label>
-                    <input type="file" name="imagen"  id="imagen" placeholder="Seleccionar la imagen" required><br />
-                </div>
-                <input type="hidden" name="accion" value="insertar_imagen"><br>
-                <input type="submit" name="enviar" value="Guardar imagen"/>
-            </form>
-        </p>
-    </section>
-</body>
-</html>
+?>
+<!-- CAMPOS DE LA TABLA : id_imagen,  titulo_imagen,  texto_imagen,  url_imagen , fecha -->
+<!-- AQUI EMPIEZA LA VISTA -->
+<section  id="ContenedorImagen">
+    <header>
+        <h1 id="titulo">Insertar imagen</h1>
+    </header>
+    <p>
+        <a class='insert' href='<?php echo site_url("imagen/lista_imagenes"); ?>'>Volver al listado</a>
+    <form class="for" enctype="multipart/form-data" action='<?php echo site_url("imagen/insertar_imagen"); ?>' method="post">
+        <input type='hidden' name='accion' value='insertar_imagen'>
+        <div>
+            <input id= "id_imagen" name='id_imagen' type ="hidden"><br />
+            <label id= "label_titulo" for="titulo">T&iacute;tulo:</label>
+            <input id="titulo" name='titulo_imagen' placeholder="Introduzca el t&iacute;tulo" required><br />
+        </div>
+        <div>
+            <label for="texto_imagen">Texto:</label>
+            <textarea id="texto_imagen" name='texto_imagen' placeholder="Introduzca la descripci&oacute;n de la imagen"></textarea><br>
+        </div>
+        <div>
+            <label for="fecha">Fecha:</label>
+            <input type="date" id="fecha" name='fecha' placeholder="Introduzca la fecha" value="<?php echo date("Y-m-d"); ?>" required><br />
+        </div>
+        <div>
+            <!-- MAX_FILE_SIZE debe preceder al campo de entrada del fichero -->
+            <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
+            <label for="imagen">Imagen:</label>
+            <input type="file" name="imagen"  id="imagen" placeholder="Seleccionar la imagen" required><br />
+        </div>
+        <input type="hidden" name="accion" value="insertar_imagen"><br>
+        <input type="submit" name="enviar" value="Guardar imagen"/>
+    </form>
+</p>
+</section>
