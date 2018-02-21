2314401175a64942e1463d4e5afbf84aae0cf394
pepeluchan <jose.luis.ramirez56@gmail.com>
2018-02-21 13:49:54 +0100

    al fin he visto la luz

--- application/models/Modeloescenas.php
+++ application/models/Modeloescenas.php
@@ -27,45 +27,43 @@
         
 
 		public function insertar() {
-			
-			$name = $this->input->post_get("name");		
-			$panorama = $this->input->post_get("panorama");
-            $cod = substr($panorama, 0 , -4);
-            $id_mapa = $_REQUEST["id_mapa"];
-            $left_mapa = $_REQUEST["left_mapa"];
-            $top_mapa = $_REQUEST["top_mapa"];
 
             $config['upload_path'] = 'assets/imagenes/escenas/';
             $config['allowed_types'] = 'jpg';
-            $config['file_name']= $panorama;
-            
+           
             $this->load->library('upload', $config);
             
             $resultado=$this->upload->do_upload('panorama');
-			
+			if($resultado) {            
+                 //////////////////////
+                 //// Se tiene que comprobar que existe ya un cod_escena igual para evitar duplicados
+                 /////////////////////
+
+                $name = $this->input->post_get("name");		
+			    $panorama = $this->upload->data()["panorama"];
+                $cod = substr($panorama, 0 , -4);
+                $id_mapa = $_REQUEST["id_mapa"];
+                $left_mapa = $_REQUEST["left_mapa"];
+                $top_mapa = $_REQUEST["top_mapa"];
+                
             
-     //////////////////////
-     //// Se tiene que comprobar que existe ya un cod_escena igual para evitar duplicados
-     /////////////////////
-            
-            
-            
-            $insert = "INSERT INTO escenas (Nombre,cod_escena,hfov,pitch,yaw,tipo,panorama) 
+                $insert = "INSERT INTO escenas (Nombre,cod_escena,hfov,pitch,yaw,tipo,panorama) 
                       VALUES('$name','$cod',120,10,10,'equirectangular','assets/imagenes/escenas/$panorama')";
-            
-            
-            
 
-            $this->db->query($insert);
-            $piso_mapa = explode("p",$id_mapa);
-            var_dump($piso_mapa);
-            $insert = "INSERT INTO puntos_mapa (nombre, left_mapa, top_mapa, id_escena, piso) 
-            VALUES ('$id_mapa',$left_mapa,$top_mapa,'$cod',$piso_mapa[1])";
-            echo $insert;
+                $this->db->query($insert);
+                $piso_mapa = explode("p",$id_mapa);
+                var_dump($piso_mapa);
+                $insert = "INSERT INTO puntos_mapa (nombre, left_mapa, top_mapa, id_escena, piso) 
+                VALUES ('$id_mapa',$left_mapa,$top_mapa,'$cod',$piso_mapa[1])";
+                echo $insert;
 
-            $this->db->query($insert);
-            
-            return $this->db->affected_rows();
+                $this->db->query($insert);
+
+                return $this->db->affected_rows();
+            }
+            else {
+                echo $this->upload->display_errors();
+            }
 		}
 
 		public function borrar ($id) {
