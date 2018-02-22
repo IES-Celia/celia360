d6b8e86d48061117b77012617d832e24f5d06c0c
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2018-02-22 19:59:12 +0100

    bibliotecaAjax

--- application/views/biblioteca/bibliotecaajax.php
+++ application/views/biblioteca/bibliotecaajax.php
@@ -1,7 +1,18 @@
+<style>
+
+	td img{
+		width:200px;
+		height:250px;
+	}
+	td{
+		margin-right:15px;
+		margin-left:15px;
+	}
+</style>
 <?php 
-	  echo "<div class='estanteria' >"; 
-            echo "<div class='nuevecica'  >"   ;    
-            echo "<table >";  
+	  echo "<div style='width:90%; margin:0 auto; ' "; 
+            echo "<div class='' >"   ;    
+            echo "<table style=''>";  
             echo "<tr>";  
 
  	$i = 0;
@@ -10,11 +21,11 @@
         $i++;
        
                      
-          echo "<td class='columna'>";
+          echo "<td class=''>";
           echo "<a href='#' ><img id='verlibro' idlibro='".$ides['id_libro']."' apaisado='".$ides['apaisado']."' class='efectBook ocultar' src='".base_url("assets/imgs/books/$ides[id_libro]/0.jpg")."' ></a>";
           echo "</td>";
       }
-          if ($i%4 == 0)  echo "</tr><tr class='torre'>";
+          if ($i%5 == 0)  echo "</tr><tr class=''>";
             }
             echo "</tr></table>";
   echo "</div>";
