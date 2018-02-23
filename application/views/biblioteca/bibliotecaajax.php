36c13beb2b1ca773f10a140d480177ea052b827b
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2018-02-23 11:06:03 +0100

    mg

--- application/views/biblioteca/bibliotecaajax.php
+++ application/views/biblioteca/bibliotecaajax.php
@@ -1,12 +1,29 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=MedievalSharp" rel="stylesheet">
+  <script src="<?php echo base_url("assets/js/jquery.js"); ?>"></script>
+   <link rel="stylesheet" href="<?php echo base_url("assets/css/ultimo-estilo.css"); ?>"/>
 <script >
-	 $('.efectBook').click(function(){
+	$(document).ready(function(){
+
+ $('.efectBook').click(function(){
+        $('.modalita2').toggle('slow');
           idlibro = $(this).attr("idlibro");
           apaisado = $(this).attr("apaisado");
-          tipo = $(this).attr("tipo");
           $('.modalita2').load('biblioteca/ver_biblioteca_ajax/'+idlibro+'/'+apaisado+'/'+tipo);
       });
+ $('#cerrarmodal').click(function(){
+        $('.modalita2').css({display:"none"});
+        $('.modalita').css({display:"block"});
+      });
+
+      $(document).keyup(function(e) {
+          if (e.keyCode == 27) { // escape key maps to keycode `27`
+            $('.modalita2').css({display:"none"});
+          }
+      });
+
+
+	}); 
 </script>
 
 <style>
@@ -47,10 +64,11 @@
 	}
 </style>
 <?php 
-	  echo "<div style='width:100%; margin:0 auto; ' "; 
+	  echo "<div style='width:100%; margin:0 auto;'"; 
             echo "<div class='headerbiblioteca' >"   ;   
             echo "<h1 style='text-align:center;'>BIBLIOTECA IES CELIA VIÑAS</h1>"; 
             echo "<img class='iniciologo' src='".base_url("assets/imagenes/portada/logo.png")."' >";
+            echo "</div>";
             echo "<table style=''>";  
             echo "<tr>";  
 
@@ -67,11 +85,12 @@
 		          if ($i%5 == 0)  echo "</tr><tr class=''>";
 		            }
 		            echo "</tr></table>";
-		  echo "</div>";
+		  
 		echo "</div>";
 
 ?>
-<div class="modalita2" style="display: none;" >
+
+<div class="modalita2" style="display: none; background:black;" >
       <div class="contenido2" style="width:1000px;background-color:white;margin:0 auto;margin-top:40px;">
         <div class="cabecera-ventana" style="background-color:white;height:60px;">
           <h1 style="text-align:center;border-bottom:1px solid black;">Titulo Libro</h1>
@@ -79,7 +98,7 @@
         <div class="cuerpo-ventana" id="cuerpo-ventana" style="margin-top:-100px;margin-bottom:100px; padding: 3%;margin-top: 80px;">
          
         </div>
-        <div class="pie-ventana23" style="border-top:1px solid black;border-radius:5px; height:50px;padding:18px;">
+        <div class="pie-ventana2" style="border-top:1px solid black;border-radius:5px; height:50px;padding:18px;">
           <a href="#" id="cerrarmodal" class="btn-2" style="float:right;">Cerrar</a>
 
         </div>
