b4e4cedeea4ad98406fb36308f3e14d9c278b7a0
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2018-02-23 10:13:55 +0100

    mg

--- application/views/biblioteca/bibliotecaajax.php
+++ application/views/biblioteca/bibliotecaajax.php
@@ -11,27 +11,36 @@
 	.tablatodo{
 		width:200px !important;
 		height:200px !important;
-		padding:25px;
+		padding:30px;
 	}
-	.pepitoo{
+	.headerbiblioteca{
 		width:100%;
 		height:115px;
 		color:white;
 		background:#000000d9;
 	}
-	.pepitoo h1{
+	.headerbiblioteca h1{
 		width: 90%;
     	padding: 20px;
     	height:54px;
 	}
 	table{
-		margin-top:80px;
+		margin-top:30px;
+	}
+	.iniciologo{
+		width:90px;
+		height:110px;
+		margin-left:30px;
+		margin-top:-110px;
 	}
 </style>
 <?php 
 	  echo "<div style='width:100%; margin:0 auto; ' "; 
-            echo "<div class='pepitoo' >"   ;   
-            echo "<h1 style='text-align:center;'>BIBLIOTECA IES CELIA VIÑAS"; 
+	  echo "<div>";
+            echo "<div class='headerbiblioteca' >"   ;   
+            	echo "<h1 style='text-align:center;'>BIBLIOTECA IES CELIA VIÑAS</h1>"; 
+            	echo "<img class='iniciologo' src='".base_url("assets/imagenes/portada/logo.png")."' >";
+            echo "</div>";	
             echo "<table style=''>";  
             echo "<tr>";  
 
@@ -42,7 +51,7 @@
 		       
 		                     
 		          echo "<td class='tablatodo'>";
-		          echo "<a href='#' ><img id='verlibro' idlibro='".$ides['id_libro']."' apaisado='".$ides['apaisado']."' class='efectBook ocultar' src='".base_url("assets/imgs/books/$ides[id_libro]/0.jpg")."' ></a>";echo "<div style='text-align:center;background:#1a76a2;color:white;margin-top:20px;height:65px;padding:15px;'>'".$ides['titulo']."'";
+		          echo "<a href='#' ><img id='verlibro' idlibro='".$ides['id_libro']."' apaisado='".$ides['apaisado']."' class='efectBook ocultar' src='".base_url("assets/imgs/books/$ides[id_libro]/0.jpg")."' ></a>";echo "<div style='text-align:center;background:#1a76a2;color:white;margin-top:20px;height:50px;padding:10px;'>'".$ides['titulo']."'";
 		          echo "</td>";
 		      }
 		          if ($i%5 == 0)  echo "</tr><tr class=''>";
