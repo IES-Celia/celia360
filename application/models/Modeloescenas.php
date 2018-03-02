7585c3bc655b44ebefff12af54ef1d870aa6ef1c
FrankLG <franlg.alm@gmail.com>
2018-03-02 09:58:37 +0100

    cambios en el borrado de escenas
    -se borran hotspots del punto
    -se borra la imagen de la escena

--- application/models/Modeloescenas.php
+++ application/models/Modeloescenas.php
@@ -67,19 +67,27 @@
 		}
 
         public function borrar($cod){
+            
+            $sql  = "DELETE FROM hotspots WHERE id_hotspot IN (
+                SELECT id_hotspot FROM escenas_hotspots where id_escena IN (
+                    SELECT id_escena FROM escenas WHERE cod_escena = '$cod'))";
+            $this->db->query($sql);
+
             $sql ="DELETE FROM puntos_mapa WHERE id_escena = '$cod'";
             $this->db->query($sql);
             
             $sql = "DELETE FROM escenas_hotspots WHERE id_escena = (SELECT id_escena FROM escenas WHERE cod_escena = '$cod') ";
             $this->db->query($sql);
 
-            $sql = "DELETE FROM escenas WHERE cod_escena = '$cod' ";
-            $this->db->query($sql);
-
             $sql = "DELETE FROM hotspots WHERE sceneid='$cod'";
             $this->db->query($sql);
 
+            $sql = "DELETE FROM escenas WHERE cod_escena = '$cod' ";
+            $this->db->query($sql);
 
+            $imagen_borrar = "assets/imagenes/escenas/$cod.JPG";
+            
+            unlink($imagen_borrar);
         
             return $this->db->affected_rows();
         } 
