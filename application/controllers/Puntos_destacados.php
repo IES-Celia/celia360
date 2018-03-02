c152673310083a2c1613170ac7461d0d1b239fd4
miguelille <32550594+miguelille@users.noreply.github.com>
2018-03-02 14:11:39 +0100

    Avances PD

--- application/controllers/Puntos_destacados.php
+++ application/controllers/Puntos_destacados.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Puntos_destacados extends CI_Controller {
     
+    public function __construct(){
+        parent::__construct();
+        $this->load->model("PuntosDestacadosModel");
+    }
+    
     public function quitar_fila($id_fila) {
         $resultado = $this->PuntosDestacadosModel->ocultar_fila($id_fila);
         if ($resultado) {
@@ -13,7 +18,6 @@ class Puntos_destacados extends CI_Controller {
             echo "Aquí generariamos la vista pero con un mensaje de error";
         }
     }
-    
     
 	public function cargar_puntosdestacados(){
         $datos["puntos_d"] = $this->PuntosDestacadosModel->getAll();
