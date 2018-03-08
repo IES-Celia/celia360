0fbb8f204799994edc0336bd835a25739f74c1b1
miguelille <32550594+miguelille@users.noreply.github.com>
2018-03-08 21:46:05 +0100

    Esto es útil :hammer:

--- application/views/escenas/Modificar.php
+++ application/views/escenas/Modificar.php
@@ -21,12 +21,12 @@
 			<fieldset class='for'>
 				<form action='".site_url("escenas/processupdatescene/")."' method='post' enctype='multipart/form-data'>
 		
-					Nombre de la Escena: <input type='text' name='name' value=" . $con['Nombre'] . "> <br/>";
+					Nombre de la Escena: <input type='text' name='name' value=" . $con['Nombre'] . "> <br/>
 				
-/*				Coordenada Pitch y Yaw:<br> 
-    <a href='".site_url('welcome/cargar_escena/'.$con['cod_escena'].'/'."update_escena_pitchyaw/")."'>Modificarlos</a><br><br>*/
+			Coordenada Pitch y Yaw:<br> 
+    <a href='".site_url('welcome/cargar_escena/'.$con['cod_escena'].'/'."update_escena_pitchyaw/")."'>Modificarlos</a><br><br>
 				
-                echo "panorama: <input type='file' name='panorama' value=" . $con['panorama'] . "> <br/>
+                panorama: <input type='file' name='panorama' value=" . $con['panorama'] . "> <br/>
 					    
 				    <input type='hidden' name='Id' value=". $con['id_escena'].">
                     <input type='hidden' name='cod' value=". $con['cod_escena'].">
