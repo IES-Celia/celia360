058c1d25e6bcdb4594212da105e5b77574866823
miguelille <32550594+miguelille@users.noreply.github.com>
2018-03-10 17:45:58 +0100

    Menu de la visita libre y guiada actualizado (añadiendo pd y biblio)

--- application/views/portada.php
+++ application/views/portada.php
@@ -338,10 +338,10 @@
             
             
             <div data-tilt data-tilt-transition="true" data-tilt-scale="1.2" class="js_tilt">
-               <div class="container_imagen">
+               <div class="container_imagen" onclick="cargardestacado()">
              <img src="<?php echo base_url("assets/imagenes/generales/puntosd.JPG");?>" class="imagen">
               <div class="overlay_imagen">
-               <p class="texto">Puntos D</p></div></div>
+               <p class="texto">Destacados</p></div></div>
               </div>
             
             </div>
@@ -349,19 +349,19 @@
             <div class="fila2">
             
             <div data-tilt data-tilt-transition="true" data-tilt-scale="1.2" class="js_tilt">
-               <div class="container_imagen">
+               <div class="container_imagen" onclick="cargarbiblioteca()">
              <img src="<?php echo base_url("assets/imagenes/generales/biblioteca.jpg");?>" class="imagen">
               <div class="overlay_imagen">
                <p class="texto">Biblioteca</p></div></div>
               </div>
-            
+            <!--
             <div data-tilt data-tilt-transition="true" data-tilt-scale="1.2" class="js_tilt">
                <div class="container_imagen">
              <img src="<?php echo base_url("assets/imagenes/generales/glosario1.jpg");?>" class="imagen">
               <div class="overlay_imagen">
                <p class="texto">Glosario</p></div></div>
               </div>
-            
+            -->
             <div data-tilt data-tilt-transition="true" data-tilt-scale="1.2" class="js_tilt">
               <!--Provisional enlace para volver atras-->
                 <a href="<?php echo site_url("index.php");?>"><div class="container_imagen">
@@ -382,6 +382,13 @@
 	
 <script type="text/javascript" src="<?php echo base_url("assets/js/slick/slick/slick.min.js");?>"></script>
 <script type="text/javascript">
+    function cargardestacado(){
+        location.href="<?php echo site_url("Puntos_destacados"); ?>";
+    }
+     function cargarbiblioteca(){
+        location.href="<?php echo site_url("biblioteca/vertodosloslibros"); ?>";
+    }
+    
 ////// AJAX CARGA JSON PANNELLUM
   
 json_contenido='';
