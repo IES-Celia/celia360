475473ae2eecad4151ae98171429f97c62b5e6b5
miguelille <32550594+miguelille@users.noreply.github.com>
2018-05-30 23:23:11 +0200

    [WIP] Añadida responsividad básica a la portada

--- application/views/portada/portada.php
+++ application/views/portada/portada.php
@@ -3,6 +3,19 @@
 <!-- También se inserta aquí el botón para visualizar los libros de historia -->
 
         <main>
+            <div id="responsividad"> <!-- WIP. Su css está en estilos_portada.css -->
+                <a href="<?php echo site_url();?>">Home</a> 
+                <a id="opcionlibre_portada" href="<?php echo site_url("tour/visita/libre"); ?>" onclick="visita_opcion('get_json_libre')" >Visita libre</a>
+                <a id="opcionguiada_portada" href="<?php echo site_url("tour/visita/guiada"); ?>" onclick="visita_opcion('get_json_guiada')" >Visita Guiada</a>
+                <a id="opciondestacada_portada" href="<?php echo site_url("PuntosDestacados"); ?>">Destacados</a>
+                 <?php 
+                 // La opción de menú "biblioteca" solo se muestra si está configurado así en las opciones de portada
+                 if ($con["show_biblioteca"]== "1") {  
+                    echo "<a id='clickbiblio' href='".site_url("biblioteca/vertodosloslibros")."'>Biblioteca</a>";
+                 }
+                 ?>
+                 <a id="creditos_portada" href="<?php echo site_url("tour/creditos"); ?>" >Créditos</a>
+            </div>
             <div id="slider1_portada" style="background-image:url('<?php echo site_url("assets/imagenes/portada/portada.jpg"); ?>')">
                  <div class="contenedor_portada">
                      <h1 id="titulito"><?php echo $con["titulo_web"] ?></h1>
