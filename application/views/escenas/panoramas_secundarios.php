4eb03bb52740a3110c94b430062a2d06878607ab
David Mora <davidmoracaceres20@gmail.com>
2018-11-29 14:25:01 +0100

    creación de vista panoramas_secundarios

--- application/views/escenas/panoramas_secundarios.php
+++ application/views/escenas/panoramas_secundarios.php
@@ -1,25 +1,33 @@
 <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
         <div id="drag_upload_file">
+        <?php 
+         if(isset($resultado))
+             echo "<h1>". $resultado."</h1>"; 
+
+         ?>
             <p>Arrastra los archivos</p>
             <p>o</p>
             <p><input type="button" class="btn btn-primary" value="Selecciona archivos" onclick="file_explorer();"></p>
             <div id="imagenesView">
 
             </div>
-            <p><button id="btnEnvio" class="btn btn-primary">Enviar</button></p>
-            <input type="file" id="selectfile" name="imagenes[]" multiple>
+
+            <input type="file" id="selectfile" name="file[]" multiple accept="image/jpeg">
+            <p><button id="btnEnvio" class="btn btn-primary" type="button">Subir imágenes</button></p>
+           
         </div>
     </div>
 
     <script>
-        var fileobj;
+
+    var fileobj = [];
     function upload_file(e) {
         e.preventDefault();
           for(i=0;i<e.dataTransfer.files.length;i++){
-            fileobj = e.dataTransfer.files[i];
-            previewfile(fileobj);
-            ajax_file_upload(fileobj);
+            fileobj.push(e.dataTransfer.files[i]);
           }
+          previewfile(fileobj);
+          ajax_file_upload(fileobj);
         }
 
  
@@ -27,59 +35,96 @@
         document.getElementById('selectfile').click();
         itemPadre = document.getElementById("drag_upload_file");
         document.getElementById('selectfile').onchange = function() {
+
           for(i=0;i<document.getElementById('selectfile').files.length;i++){
-            fileobj = document.getElementById('selectfile').files[i];
-            fileobj.name = i+".jpg"
-            previewfile(fileobj);
-            ajax_file_upload(fileobj);
+            fileobj.push(document.getElementById('selectfile').files[i]);
           }
+          previewfile(fileobj);
+          ajax_file_upload(fileobj);
         };
     }
     var contador = 1;
+    var acumulable = 0;
+
     function previewfile(file) {
-        var reader = new FileReader();
-        reader.onload = function (event) {
-          var image = new Image();
-          var inputName = document.createElement("input");
-          var label = document.createElement("label");
-          var label2 = document.createElement("label");
-          var fecna = document.createElement("input");
-          fecna.setAttribute("type",'date');
-          fecna.setAttribute('name','fecha'+contador);
-          var contentLabel = document.createTextNode("Título de imágen "+contador+": ");
-          var contentLabelFecna = document.createTextNode("Fecha de imágen "+contador+": ");
-          label.setAttribute("for","nombre"+contador);
-          label.appendChild(contentLabel);
-          label2.setAttribute("for","fecha"+contador);
-          label2.appendChild(contentLabelFecna);
-          inputName.setAttribute("type","text");
-          inputName.setAttribute("name","nombre"+contador);
-          contador++;
-          image.src = event.target.result;
-          image.width = 250; // a fake resize
-          image.height = 250;
-          document.getElementById("imagenesView").appendChild(label);
-          document.getElementById("imagenesView").appendChild(inputName);
-          document.getElementById("imagenesView").appendChild(label2);
-          document.getElementById("imagenesView").appendChild(fecna);
-          document.getElementById("imagenesView").appendChild(image);
-        };
-        reader.readAsDataURL(file);
+      for(i = 0;i<file.length;i++){
+        if(file[i].name.includes("jpg")){
+          var reader = new FileReader();
+          reader.onload = function (event) {
+            var image = new Image();
+            var inputName = document.createElement("input");
+            var label = document.createElement("label");
+            var label2 = document.createElement("label");
+            var fecna = document.createElement("input");
+            fecna.setAttribute("type",'date');
+            fecna.setAttribute('name','fecha'+contador);
+            fecna.setAttribute("id","fecha"+contador);
+            var contentLabel = document.createTextNode("Título de imágen "+contador+": ");
+            var contentLabelFecna = document.createTextNode("Fecha de imágen "+contador+": ");
+            label.setAttribute("for","nombre"+contador);
+            label.appendChild(contentLabel);
+            label2.setAttribute("for","fecha"+contador);
+            label2.appendChild(contentLabelFecna);
+            inputName.setAttribute("type","text");
+            inputName.setAttribute("name","nombre"+contador);
+            inputName.setAttribute("id","nombre"+contador);
+            contador++;
+            image.src = event.target.result;
+            image.width = 250; // a fake resize
+            image.height = 250;
+            document.getElementById("imagenesView").appendChild(label);
+            document.getElementById("imagenesView").appendChild(inputName);
+            document.getElementById("imagenesView").appendChild(label2);
+            document.getElementById("imagenesView").appendChild(fecna);
+            document.getElementById("imagenesView").appendChild(image);
+            acumulable++;
+          }
+          reader.readAsDataURL(file[i]);
+        }
+
+        
       }
+    }
  
     function ajax_file_upload(file_obj) {
       document.getElementById("btnEnvio").addEventListener('click',function(){
-        if(file_obj != undefined) {
-            var form_data = new FormData();                  
-            form_data.append('file', file_obj);
+            var form_data = new FormData();
+            for(var i = 0;i<file_obj.length;i++){                 
+              form_data.append('file[]', file_obj[i]);
+            }
+
+            for(var j = 1;j<=acumulable;j++){
+              form_data.append('nombre'+j,document.getElementById("nombre"+j).value);
+              form_data.append('fecha'+j,document.getElementById("fecha"+j).value);
+            }
+            var myNode = document.getElementById("imagenesView");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
             $.ajax({
                 type: 'POST',
-                url: '<?php base_url("Escenas/insertSecondaryPanorama") ?>',
+                url: '<?php echo site_url('Panoramas_Secundarios/insertSecondaryPanorama/'.$datos_escena[0]['id_escena']); ?>',
                 contentType: false,
                 processData: false,
-                data: form_data
+                data: form_data,
+                success:function(result){
+                  console.log(result);
+
+                  if(result == 0){
+                    document.getElementById("mensaje_cabecera").innerHTML = "Imágenes subidas con éxito";
+                   
+                  }else{
+                    document.getElementById("error_cabecera").innerHTML = "Error al insertar todas las imágenes";
+                   
+                  }
+                }
             });
-        }
-      });
+        });
+        fileobj = [];
+        contador = 1;
+        acumulable = 0;
+        form_data: "";
     }
+
+    
     </script>
