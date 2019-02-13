0d776955d3b264ceddbc3f03c57bfeefa9bf1d9c
daniel <danisierro95@gmail.com>
2019-02-13 10:19:48 +0100

    Vistar footer
    Refactorizando vistas

--- application/views/admin_footer.php
+++ application/views/admin_footer.php
@@ -1,7 +1,11 @@
 
 
-<footer>
-<p>&copy; IES Celia Viñas 2018</p>
-</footer>
+    <footer>
+        <div class="row mt-3">
+            <div class="col-md-12">
+                <p class="text-center">&copy; IES Celia Viñas 2018</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
