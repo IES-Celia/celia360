9ddfb9bd52814f1191d3185ef540511f583fcd7f
David Mora <davidmoracaceres20@gmail.com>
2019-02-13 12:13:13 +0100

    add

--- application/views/admin_footer.php
+++ application/views/admin_footer.php
@@ -3,10 +3,9 @@
 	<!-- Bootstrap JS -->
 	<script src="<?php echo base_url('assets/js/bootstrap/js/bootstrap.min.js'); ?>"></script>
 	<script src="<?php echo base_url('assets/js/bootstrap/js/popper.min.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/bootstrap/js/jquery-3.3.1.slim.min.js'); ?>"></script>
 	<script src="<?php echo base_url("assets/js/jquery.dataTables.min.js"); ?>"></script>
 	
-	
+
     <div class="container-fluid mt-3">
         <div class="row">
             <div class="col-md-12">
