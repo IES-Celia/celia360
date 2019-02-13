7bfed64c8965e8b9c110f11a7a7ea2170fb18808
daniel <danisierro95@gmail.com>
2019-02-13 12:23:00 +0100

    Vista biblioteca
    Refactorizando vista

--- application/views/biblioteca/intadmin.php
+++ application/views/biblioteca/intadmin.php
@@ -1,98 +1,129 @@
- <link rel='stylesheet' href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css>
-    
+<link rel='stylesheet' href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css> <style
+    type="text/css">
 
-    <style type="text/css">
-           
-        label{
-        
-            margin-left: 10px;
-        
-        }
-        
-        #caja input[type=text]{
-            
-            width:70%;            
-        }
-        
-        .img-cerrar{
-         
-            width: 20px; height: 20px;
-        }
- 
-        .cerrar{
-            
-            position:relative;
-            top:-428px;
-            left:45%;
-        }
-        
-        .cerrar1{
-            position:relative;
-            top:-388px;
-            left:45%;
-        }
-        .cerrarBorrar{
-            position:relative;
-            top:-192px;
-            left:47%;
-        }
-    </style>
-    <div>
-        <?php
+label{
 
-          
-            echo"<a class='insert' onclick='mostrar()' > <i class='fas fa-plus-circle'></i> Insertar Libro</a>"; 
-            echo "<table id='cont' class='display' style='margin-top:10px;'>";
-                    echo "<thead><tr id='cabecera'>
-                            <th>Id</th>
-                            <th>Titulo</th>
-                            <th>Autor</th>
-                            <th>Editorial</th>
-                            <th>Lugar de edicion</th>
-                            <th>Fecha de edicion</th>
-                            <th>ISBN</th>
-                            <th>Tipo</th>
-                            <th>Apaisado</th>
-                            <th>Modificar</th>
-                            <th>Páginas</th>
-                            <th>Eliminar</th>
-                            <th>Subir</th>
-                            
-                           </tr></thead>";
+margin-left: 10px;
 
+}
 
-               
+#caja input[type=text]{
+
+width:70%;
+}
+
+.img-cerrar{
+
+width: 20px; height: 20px;
+}
+
+.cerrar{
+
+position:relative;
+top:-428px;
+left:45%;
+}
+
+.cerrar1{
+position:relative;
+top:-388px;
+left:45%;
+}
+.cerrarBorrar{
+position:relative;
+top:-192px;
+left:47%;
+}
+</style>
+
+    <div class="container">
+
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <a name="" id="" class="float-right btn btn-primary" href="#" role="button" onclick="mostrar()"><i
+                        class='fas fa-plus-circle'></i> Insertar Libro</a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table display bg-secondary" id='cont'>
+                    <thead class="text-center">
+                        <tr id='cabecera'>
+                            <th class="align-middle">Id</th>
+                            <th class="align-middle">Titulo</th>
+                            <th class="align-middle">Autor</th>
+                            <th class="align-middle">Editorial</th>
+                            <th class="align-middle">Lugar de edicion</th>
+                            <th class="align-middle">Fecha de edicion</th>
+                            <th class="align-middle">ISBN</th>
+                            <th class="align-middle">Tipo</th>
+                            <th class="align-middle">Apaisado</th>
+                            <th class="align-middle">Modificar</th>
+                            <th class="align-middle">Páginas</th>
+                            <th class="align-middle">Eliminar</th>
+                            <th class="align-middle">Subir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                 /*echo "<a href='index.php?accion=showinsertlibro'><i class='fa fa-plus' aria-hidden='true'></i><i title='Insertar libro' class='fa fa-book' aria-hidden='true'></i></a>";*/
                 foreach ($tabla as $usu) {
                     $id = $usu['id_libro'];
                     echo "<tr id='libro".$id."'>
-                            <td >".$usu["id_libro"]."</td>
-                            <td id='titulo_libro_".$id."'>".$usu["titulo"]."</td>
-                            <td id='autor_libro_".$id."'>".$usu["autor"]."</td>
-                            <td id='editorial_libro_".$id."'>".$usu["editorial"]."</td>
-                            <td id='lugar_edicion_".$id."'>".$usu["lugar_edicion"]."</td>
-                            <td id='fecha_edicion_".$id."'>".$usu["fecha_edicion"]."</td>
-                            <td id='isbn_libro_".$id."'>".$usu["ISBN"]."</td>";
+                            <td class='align-middle'>".$usu["id_libro"]."</td>
+                            <td class='align-middle' id='titulo_libro_".$id."'>".$usu["titulo"]."</td>
+                            <td class='align-middle' id='autor_libro_".$id."'>".$usu["autor"]."</td>
+                            <td class='align-middle' id='editorial_libro_".$id."'>".$usu["editorial"]."</td>
+                            <td class='align-middle' id='lugar_edicion_".$id."'>".$usu["lugar_edicion"]."</td>
+                            <td class='align-middle' id='fecha_edicion_".$id."'>".$usu["fecha_edicion"]."</td>
+                            <td class='align-middle' id='isbn_libro_".$id."'>".$usu["ISBN"]."</td>";
                             if($usu["tipo"]==0){
-                                echo "<td>Biblioteca</td>";
+                                echo "<td class='align-middle'>Biblioteca</td>";
                               }else{
-                                echo "<td>Historia</td>";
+                                echo "<td class='align-middle'>Historia</td>";
                               }
                               if($usu["apaisado"]==0){
-                                echo "<td>Normal</td>";
+                                echo "<td class='align-middle'>Normal</td>";
                               }else{
-                                echo "<td>Apaisado</td>";
+                                echo "<td class='align-middle'>Apaisado</td>";
                               }                            
-                            echo" <td>
+                            echo" <td class='align-middle'>
                             <a onclick='mostrarm(".$usu['id_libro'].")'> <i class='fa fa-edit' style='font-size:20px;'></i></a>
-                            <td><a href='".site_url("/biblioteca/showinsertimg/".$usu["id_libro"])."'><i class='fas fa-file-alt' style='font-size:20px;'></i></a></td>
-                            <td><a href='#' onclick='mostrarborrar(".$usu['id_libro'].")'><i title='Eliminar' class='fa fa-trash' aria-hidden='true'></i></a></td>
-                            <td><a href='".site_url("/biblioteca/showsubida/".$usu["id_libro"])."'><i class='fa fa-upload' aria-hidden='true'></i></a></td>
+                            <td class='align-middle'><a href='".site_url("/biblioteca/showinsertimg/".$usu["id_libro"])."'><i class='fas fa-file-alt' style='font-size:20px;'></i></a></td>
+                            <td class='align-middle'><a href='#' onclick='mostrarborrar(".$usu['id_libro'].")'><i title='Eliminar' class='fa fa-trash' aria-hidden='true'></i></a></td>
+                            <td class='align-middle'><a href='".site_url("/biblioteca/showsubida/".$usu["id_libro"])."'><i class='fa fa-upload' aria-hidden='true'></i></a></td>
                             
 
                            </tr>";
                 }
-                echo "</table>";
+?>
+                    </tbody>
+                    <tfoot class="text-center">
+                        <tr id='cabecera'>
+                            <th class='align-middle'>Id</th>
+                            <th class='align-middle'>Titulo</th>
+                            <th class='align-middle'>Autor</th>
+                            <th class='align-middle'>Editorial</th>
+                            <th class='align-middle'>Lugar de edicion</th>
+                            <th class='align-middle'>Fecha de edicion</th>
+                            <th class='align-middle'>ISBN</th>
+                            <th class='align-middle'>Tipo</th>
+                            <th class='align-middle'>Apaisado</th>
+                            <th class='align-middle'>Modificar</th>
+                            <th class='align-middle'>Páginas</th>
+                            <th class='align-middle'>Eliminar</th>
+                            <th class='align-middle'>Subir</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+
+    </div><!-- Final de container -->
+
+
+    <?php
 
                 //Modal confirmacion eliminar libro
                 echo"<div id='eliminarLibro'>
@@ -290,100 +321,89 @@
                 ";
                  echo "</div>";
                  echo "</div>";
+?>
+    <script>
+        function respuesta(r) {
+            if (r == 0) {
+                alert("Error");
+            } else {
+                selector = "#libro" + parseInt(r);
+                $(selector).remove();
+            }
+        }
 
-    ?>
+        function mostrarm(id_libro) {
+            titulo = document.getElementById("titulo_libro_" + id_libro).innerHTML;
+            autor = document.getElementById("autor_libro_" + id_libro).innerHTML;
+            editorial = document.getElementById("editorial_libro_" + id_libro).innerHTML;
+            lugar_edicion = document.getElementById("lugar_edicion_" + id_libro).innerHTML;
+            fecha_edicion = document.getElementById("fecha_edicion_" + id_libro).innerHTML;
+            isbn = document.getElementById("isbn_libro_" + id_libro).innerHTML;
+            pasar_id = id_libro;
 
-     <script >
+            $("#modif_titulo").val(titulo);
+            $("#modif_autor").val(autor);
+            $("#modif_editorial").val(editorial);
+            $("#modif_lugar_edicion").val(lugar_edicion);
+            $("#modif_fecha_edicion").val(fecha_edicion);
+            $("#modif_isbn").val(isbn);
+            $("#modif_id_libro").val(pasar_id);
 
+            $("#modificar").show();
+        }
 
-            function respuesta(r){
-                if(r==0){
-                    alert("Error");
-                }else{
-                    selector = "#libro"+parseInt(r);
-                    $(selector).remove();
+        function mostrarborrar(id_libro) {
+
+            b_idlibro = id_libro;
+            $("#borrar_id_libro").val(b_idlibro);
+            $("#idlibroborrar").text(b_idlibro);
+
+            $("#eliminarLibro").show();
+        }
+
+        function borrarlibro() {
+            id_libro = $("borrar_id_libro").val();
+            alert(id_libro);
+            confirmacion = confirm("¿Estas seguro que desea borrar el libro?");
+
+            if (confirmacion == true)
+                $.get("<?php echo base_url('biblioteca/deletelibro/');?>" + id_libro, null, respuesta);
+
+        }
+
+        function mostrar() {
+            $("#insertar").show();
+
+        }
+
+        function alertid(id_libro) {
+            alert("EL nombre de la carpeta que contenga el libro debera ser: " + id_libro);
+        }
+        function cerrar() {
+
+            $("#insertar").hide();
+            $("#modificar").hide();
+            $("#eliminarLibro").hide();
+
+        }
+
+        $(document).ready(function () {
+            $('#cont').dataTable({
+                "language": {
+                    "lengthMenu": "Mostrar _MENU_ registros por página",
+                    "zeroRecords": "No se encontraron resultados en su búsqueda",
+                    "searchPlaceholder": "Buscar registros",
+                    "info": "Mostrando registros de _START_ al _END_ de un total de  _TOTAL_ registros",
+                    "infoEmpty": "No existen registros",
+                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "search": "Buscar:",
+                    "paginate": {
+                        "first": "Primero",
+                        "last": "Último",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
                 }
-            }
-
-            function mostrarm(id_libro){
-                titulo = document.getElementById("titulo_libro_"+id_libro).innerHTML;
-                autor = document.getElementById("autor_libro_"+id_libro).innerHTML;
-                editorial = document.getElementById("editorial_libro_"+id_libro).innerHTML;
-                lugar_edicion = document.getElementById("lugar_edicion_"+id_libro).innerHTML;
-                fecha_edicion = document.getElementById("fecha_edicion_"+id_libro).innerHTML;
-                isbn = document.getElementById("isbn_libro_"+id_libro).innerHTML;
-                pasar_id=id_libro;
-
-                $("#modif_titulo").val(titulo);
-                $("#modif_autor").val(autor);
-                $("#modif_editorial").val(editorial);
-                $("#modif_lugar_edicion").val(lugar_edicion);
-                $("#modif_fecha_edicion").val(fecha_edicion);
-                $("#modif_isbn").val(isbn);
-                $("#modif_id_libro").val(pasar_id);
-
-                $("#modificar").show();
-            }
-
-            function mostrarborrar(id_libro){
-
-                b_idlibro=id_libro;
-                $("#borrar_id_libro").val(b_idlibro);
-                $("#idlibroborrar").text(b_idlibro);
-
-                $("#eliminarLibro").show();
-            }
-
-            function borrarlibro(){
-                id_libro=$("borrar_id_libro").val();
-                alert(id_libro);
-                confirmacion=confirm("¿Estas seguro que desea borrar el libro?");
-
-                if(confirmacion==true)
-                  $.get("<?php echo base_url('biblioteca/deletelibro/');?>"+id_libro,null,respuesta);
-
-            }
-
-            function mostrar(){
-                $("#insertar").show();
-                
-            }
-
-            function alertid(id_libro){
-               alert("EL nombre de la carpeta que contenga el libro debera ser: "+id_libro);
-            }
-            function cerrar(){
-              
-                $("#insertar").hide();
-                $("#modificar").hide();
-                $("#eliminarLibro").hide();
-
-            }  
-
-            $(document).ready(function() {
-              $('#cont').dataTable({
-                  "language": {
-                        "lengthMenu": "Mostrar _MENU_ registros por página",
-                        "zeroRecords": "No se encontraron resultados en su búsqueda",
-                        "searchPlaceholder": "Buscar registros",
-                        "info": "Mostrando registros de _START_ al _END_ de un total de  _TOTAL_ registros",
-                        "infoEmpty": "No existen registros",
-                        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-                        "search": "Buscar:",
-                        "paginate": {
-                          "first":    "Primero",
-                          "last":    "Último",
-                          "next":    "Siguiente",
-                          "previous": "Anterior"
-                        },
-                  }
-              });
-            } );
-        
+            });
+        });
     </script>
-
-</div>
-
-
-
-
