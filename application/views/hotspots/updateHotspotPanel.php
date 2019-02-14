e624918d1dda2ce721f2b1a573f33c0d563faf44
David Mora <davidmoracaceres20@gmail.com>
2019-02-14 12:49:45 +0100

    add

--- application/views/hotspots/updateHotspotPanel.php
+++ application/views/hotspots/updateHotspotPanel.php
@@ -1,95 +1,80 @@
-<html>
-	<head>
-	<style type="text/css">
+  <div class="container mt-2">
+		  <div class="row">
+			  <div class="col-md-12">
+				  <h1 class="text-center">Modificación de panel de información</h1>
+			  </div>
+		  </div>
+		  <div class="row">
 
-.button {
-    background-color: #555555; /* Black	*/
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-	
-	}
-	
-div.centrado {
-	margin-left:25%;
-	margin-right:25%;
-	
-}
+		<div class="col-md-12">
 
+		  <div class="card">
+			  <div class="card-body">
+				  <div class="col-md-12">
 
-    #caja6{
-        transform: translate(-50%, -42%);
-       
-        }
-        
-    #caja6 a{
-        max-width: 580px !important;
-        width: 580px !important;
-    }
-        
-
-
-        .button{
-            width: 580px !important;
-            border: 3px solid white;
-		}
-		
-		.blanco {
-			color:white;
-		}
-
-</style>
-		<title> Modificar </title>
-</head>
-<body>
-
-<?php
-
+				  
+				  <?php
 $tabla = $tabla[0];
 echo "
 
-<h1 class='blanco'> Formulario para UPDATE HotspotsPanel </h1>
-
-<fieldset id='caja6'>
-
 <form action=' ".site_url("hotspots/process_update_hotspot/".$escena_inicial.'/'.$tipo_update)." ' method='get'>
 
-	Coordenadas donde se situa el punto:<br> 
-    <a class='link' href='".site_url('escenas/cargar_escena_modificar/'.$codigo_escena.'/'."update_hotspot_pitchyaw/".$tabla['id_hotspot'])."'>Modificarlos</a><br><br>
-    
-	Título panel:<br> <input style='margin-top:5px;' type='text' value='".$tabla['titulo_panel']."' name='titulo_panel'> </br> </br>
-    Texto panel: <input type='hidden' id='texto_panel' value='' name='texto_panel'> </br> </br>
-	<div id='editor'>
+<div class='form-group'>
+  	<label for='coords'>Coordenadas donde se situa el punto</label>
+	<a class='link btn btn-primary' href='".site_url('escenas/cargar_escena_modificar/'.$codigo_escena.'/'."update_hotspot_pitchyaw/".$tabla['id_hotspot'])."'>Modificarlos</a>
+</div>
+
+  <div class='form-group'>
+  <label for='title'>Título panel</label>
+	 <input class='form-control' type='text' value='".$tabla['titulo_panel']."' name='titulo_panel'>
+  </div>
+	 
+  <div class='form-group'>
+	  <label for='text'>Texto panel</label>
+	  <input type='hidden' id='texto_panel' value='' name='texto_panel'>
+	  <div id='editor'>
 		".$tabla['texto_panel']."
 	</div>
+  </div>
+
+     
+	
 	<input type='hidden' name='id_hotspot' value='".$tabla['id_hotspot']."'>
     <input type='hidden' name='cssClass' value='".$tabla['cssClass']."'>
     <input type='hidden' name='pitch' value='".$tabla['pitch']."'>
     <input type='hidden' name='yaw' value='".$tabla['yaw']."'>
     
-	
-    <input type='submit' class='button'>
-	<br>
-    <a href=
+	<div class='form-group'>
+	<input type='submit' class='button btn btn-success'>
+	<a href=
     '".site_url("/hotspots/delete_hotspot/".$tabla['id_hotspot']."/".$tipo_update)."'
-    class='rojo_borrar link' >BORRAR ESTE HOTSPOT (CUIDADO!)</a></td>
+    class='rojo_borrar btn btn-danger link' >BORRAR ESTE HOTSPOT (CUIDADO!)</a>
+	</div>
+
+	<div class='form-group'>
+	
+	</div>
+	
 	
 </form>
 <form action=' ".site_url("hotspots/modify_panel_info/".$tabla['id_hotspot'].'/'.$tipo_update.'/'.$escena_inicial)." ' method='post'>
 <input type='hidden' name='id_scene' value='".$codigo_escena."'>
-<input type='submit' class='button' value='Modificar imagenes de este hotspot'>
+<div class='form-group'><input type='submit' class='button btn btn-info' value='Modificar imagenes de este hotspot'></div>
+
 </form>
-</fieldset>
 
 "; /**  Cierre echo **/
 
 ?>
+				  </div>
+			  </div>
+		  </div>
 
-<script>
+		  </div>
+	  </div>
+	</div>
+
+	<script>
         var quill = new Quill('#editor', {
             modules: {
         toolbar: [
@@ -124,4 +109,4 @@ echo "
             document.getElementById('texto_panel').value = quill.getHtml();
         });
 
-      </script>
+	  </script>
